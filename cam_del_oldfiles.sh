4e126e17a43ebfce127a58c44c6537470e1066d0
msonner <manfred.sonner@icloud.com>
2018-12-26 15:35:36 +0100

    problems with properties solved, scripts no chown before deleting, moving...

--- cam_del_oldfiles.sh
+++ cam_del_oldfiles.sh
@@ -1,8 +1,10 @@
-# !.sh
+# !/bin/sh
 # script deletes all pictures and movie older than x days 
 # (motion generated ones)
+# expects no parameters
 IMAGE_DIR=/home/pi/Bilder/cam
 # number of days, if this not works use -mmin 360 (e.g. 6 hours)
 # option -delele is missing, for security reason
+sudo chown pi $IMAGE_DIR/*.*
 find $IMAGE_DIR -mtime +1 -type f -delete 
 
--- cam_for_ios.sh
+++ cam_for_ios.sh
@@ -1,6 +1,7 @@
-# !.sh
+# !/bin/sh
 # this script converts all .avi from motion to .m4v (ios) und puts them
-# into the NAS - important NAS has to mounted (mount -a) 
+# into the NAS - important NAS has to be mounted (mount -a)
+# script expects no parameter 
 # directory with images 
 IMAGE_DIR=/home/pi/Bilder/cam
 # buffer directory for .avi files - perhaps not needed at all
@@ -9,9 +10,11 @@ BUFFER_DIR=/home/pi/Bilder/cam_transfer/buffer
 NAS_DIR=/home/pi/fritz_speicher/
 
 # move all .avi in buffer directory
+# TOD replace path  here
 cd $IMAGE_DIR
 for file in *.avi; do
-	cp  "$file" "/home/pi/Bilder/cam_transfer/buffer"
+	sudo chown pi "$file"; 
+	mv  "$file" "/home/pi/Bilder/cam_transfer/buffer"
 done 
 
 # convert all .avi in ios readable format, -y should force overwriting
@@ -19,7 +22,8 @@ done
 # strip the ugly .avi extension from filename before
 cd $BUFFER_DIR
 for file in *.avi; do
-	ffmpeg -y -i "$file" $NAS_DIR"${file%%.avi}.m4v"
+	sudo chown pi "$file";
+	ffmpeg -y -i "$file" $NAS_DIR"${file%%.avi}.m4v"; rm "$file"
 done
 # TODO rm all *._* buffer files in NAS_DIR, means use mv instead of cp above
 # TODO log everything
